3e26210e250602f67ed4efc593a0aba3ef6bf453
ruhanimittal <rmittal7@student.ubc.ca>
2024-11-10 14:25:50 -0800

    home

--- hackcamp/src/components/HeroSection/hero.js
+++ hackcamp/src/components/HeroSection/hero.js
@@ -1,16 +1,25 @@
 import React, {useState} from "react";
 import './hero.css';
 import { Link } from 'react-scroll';
+import Tree from "../../assets/tree.png";
+import Bird from "../../assets/image.png";
 
 const HeroSection = () => {
 
-    // const [showMenu, setShowMenu] = useState(false);
-
     return (
         <section id="Home">
-            <div className="main">
-                
+            <div className="circle-container">
+            <div className="quarter-circle top-left"></div>
+            <div className="quarter-circle top-right"></div>
+            <div className="quarter-circle bottom-left"></div>
+            <div className="quarter-circle bottom-right"></div>
+            <div className="main-circle">
+                <img src={Tree} alt="tree" className="tree" />
             </div>
+            <div className="bird">
+                <img src={Bird} alt="bird" className="bird-img" />
+            </div>
+        </div>
         </section>
     );
 }
--- hackcamp/src/components/Navbar/navbar.js
+++ hackcamp/src/components/Navbar/navbar.js
@@ -8,10 +8,16 @@ const Navbar = () => {
 
     return (
         <nav className="navbar fixed-top">
+            <span className="title">WellSpring</span>
                 <div className="desktopMenu">
+                    <Link activeClass="active" to="" className="desktopMenuListItem">HOME</Link>
+                    <Link activeClass="active" to="" className="desktopMenuListItem">DASHBOARD</Link>
                     <Link activeClass="active" to="" className="desktopMenuListItem">RESOURCES</Link>
                     <Link activeClass="active" to="" className="desktopMenuListItem">SAY HELLO ;)</Link>
                 </div>
+            <div className="button">
+                <span className="Sign-in">SIGN IN</span>
+            </div>
         </nav>
     )
 }
