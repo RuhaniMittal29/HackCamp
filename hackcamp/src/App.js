3a06fa7f4d5214cb8e9712843167b51e9f1ff689
ruhanimittal <rmittal7@student.ubc.ca>
2024-11-10 12:25:15 -0800

    some change

--- hackcamp/src/App.js
+++ hackcamp/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/navbar";
+import Home from "./components/HeroSection/hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ hackcamp/src/components/HeroSection/hero.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import './hero.css';
+import { Link } from 'react-scroll';
+
+const HeroSection = () => {
+
+    // const [showMenu, setShowMenu] = useState(false);
+
+    return (
+        <section id="Home">
+            <div className="main">
+                
+            </div>
+        </section>
+    );
+}
+
+export default HeroSection;
