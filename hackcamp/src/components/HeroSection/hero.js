d6f5a652a1c84d0743f1b098d8aba7bb2ade7254
Ruhani Mittal <86153948+RuhaniMittal29@users.noreply.github.com>
2024-11-10 18:21:57 -0800

    Update hero.js

--- hackcamp/src/components/HeroSection/hero.js
+++ hackcamp/src/components/HeroSection/hero.js
@@ -1,6 +1,5 @@
-import React, {useState} from "react";
+import React from "react";
 import './hero.css';
-import { Link } from 'react-scroll';
 import Tree from "../../assets/tree.png";
 import Bird from "../../assets/image.png";
 import Water from "../../assets/water-home.png";
@@ -13,17 +12,29 @@ const HeroSection = () => {
     return (
         <section id="Home">
             <div className="circle-container">
-            <div className="quarter-circle top-left">
+            <div className="quarter-circle top-left food-water">
                 <img src={Water} alt="water" className="quarter-img" />
+                <div className="text-box">
+                        <span className="text">Water💧 helps keep your energy up⬆️, supports digestion, and keeps your body working well. Drinking 2L of water a day prevents problems like headaches🤕 and tiredness😩 by helping your body flush out toxins💩.</span>
+                    </div>
             </div>
-            <div className="quarter-circle top-right">
+            <div className="quarter-circle top-right sleep-fitness">
                 <img src={Sleep} alt="sleep" className="quarter-img" />
+                <div className="text-box">
+                        <span className="text">Getting enough sleep😴 helps your body and mind recharge🔋, boosting energy, focus, and mood. 8 hours of sleep per night also supports your immune system, memory, and overall health👩🏻‍⚕️.                        </span>
+                    </div>
             </div>
-            <div className="quarter-circle bottom-left">
+            <div className="quarter-circle bottom-left food-water">
                 <img src={Food} alt="sleep" className="quarter-img" />
+                <div className="text-box">
+                        <span className="text">Good nutrition🥬 gives your body the nutrients🥑 it needs for energy, growth, and repair, helping you feel and perform your best💃🏻. Eating three balanced meals a day also reduces the risk of chronic diseases like heart disease❤️ and diabetes.                        </span>
+                    </div>
             </div>
-            <div className="quarter-circle bottom-right">
+            <div className="quarter-circle bottom-right sleep-fitness">
                 <img src={Fitness} alt="sleep" className="quarter-img" />
+                <div className="text-box">
+                        <span className="text">Regular exercise🏃🏻‍♀️ strengthens your muscles💪🏻, boosts energy🔋, and supports mental well-being🤗. Staying active for 30 minutes a day also improves heart health❤️, helps manage weight, and reduces the risk of chronic diseases 🤧.                        </span>
+                    </div>
             </div>
             <div className="main-circle">
                 <img src={Tree} alt="tree" className="tree" />
